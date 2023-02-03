e22bf570513e63074f711e0dd5994273369e0064
jeanchaneto <92741347+jeanchaneto@users.noreply.github.com>
2023-02-03 08:41:27 +0100

    Setup router/file structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Header from './components/Header';
+import SideBar from './components/SideBar';
+import Dashboard from './Dashboard';
 
 function App() {
   return (
-    <div className="App">
-      <div>App</div>
-    </div>
+    <Router>
+      <Header />
+      <main>
+        <SideBar />
+        <Routes >
+          <Route index element={<Dashboard />} />
+        </Routes>
+      </main>
+    </Router>
   );
 }
 
